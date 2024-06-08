dbeece5ebe11eb8bf1af035b434b0c88db379c6c
suoeh <dylan.x4.com@gmail.com>
2024-06-08 14:40:45 -0400

    PRAY

--- test.py
+++ test.py
@@ -2,19 +2,19 @@ import requests
 import json
 import random
 
-data = {
-    'key1': str(random.randint(1, 100)),
-    'key2': str(random.randint(1, 100))
-}
+url = 'http://127.0.0.1:5000/receive_progress'
+data = 'GRAHH'
 
-url = 'http://127.0.0.1:5000/receive_data'
-response = requests.post(url, json=data)
-
-# print("Response status code:", response.status_code)
-# print("Response content:", response.text)
-
-# try:
-#     response_json = response.json()
-#     print(response_json)
-# except requests.exceptions.JSONDecodeError as e:
-#     print("Failed to parse response as JSON:", e)
+while 1:
+    cmd = input()
+    if cmd == '0':
+        data = random.randint(1, 100)
+        url = 'http://127.0.0.1:5000/receive_progress'
+        response = requests.post(url, json=data)
+    elif cmd == '1':
+        data = 'Current song : snow bunny heaven'
+        url = 'http://127.0.0.1:5000/receive_song'
+        response = requests.post(url, json=data)
+    else:
+        print("QUIT GG")
+        break
--- webstitecolon3/app.py
+++ webstitecolon3/app.py
@@ -1,29 +1,78 @@
 from flask import *
+from flask_apscheduler import APScheduler
 import requests
 import json
 
 app = Flask(__name__)
 
+counter = 0
+received_data = "loading!"
 
-received_data = {}
+def get_data():
+    global counter
+    counter += 1
+    return str(counter)
 
-@app.route('/receive_data', methods=['POST'])
-def receive_data():
+# buttons ish
+@app.route('/')
+def index():
+    counter = get_data()
+    return render_template('index.html',counter=counter)
+
+@app.route('/play_menu')
+def play_game_menu():
+    return render_template('play_menu.html')
+
+@app.route('/play_menu/vocal_game')
+def vocal_game():
+    return render_template('vocal_game.html')
+
+@app.route('/play_menu/guitar_game')
+def guitar_game():
+    return render_template('guitar_game.html')
+
+@app.route('/play_menu/piano_game')
+def piano_game():
+    return render_template('piano_game.html')
+
+
+
+# progress code
+song = 'green'
+
+@app.route('/receive_progress', methods=['POST'])
+def receive_progress():
     global received_data
     try:
         received_data = request.json
-        return jsonify({'status': 'success', 'received_data': received_data})
+        return jsonify({'status': 'success', 'received_progress': received_data})
     except Exception as e:
         app.logger.error(f"Error receiving data: {e}")
         return jsonify({'status': 'error', 'message': str(e)}), 500
-@app.route('/get_data', methods=['GET'])
-def get_data():
-    return jsonify(received_data)
     
+@app.route('/get_progress', methods=['GET'])
+def get_progress():
+    return jsonify(received_data)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# song code
 
+@app.route('/receive_song', methods=['POST'])
+def receive_song():
+    global song
+    try:
+        song = request.json
+        return jsonify({'status': 'success', 'received_song': song})
+    except Exception as e:
+        app.logger.error(f"Error receiving data: {e}")
+        return jsonify({'status': 'error', 'message': str(e)}), 500
+
+@app.route('/get_song', methods=['GET'])
+def get_song():
+    return jsonify(song)
+
+# jfdklf
 if __name__ == '__main__':
+    scheduler = APScheduler()
+    scheduler.add_job(func=get_data,trigger='interval',id='job',seconds = 1)
+    scheduler.start()
     app.run(debug=True)
