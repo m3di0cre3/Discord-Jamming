f1f5349e01d6449964456836eef032003d7dee7c
Owen Wu <owenwdd01@gmail.com>
2024-06-08 01:50:22 -0400

    trying to get this to work

--- discord_bot.py
+++ discord_bot.py
@@ -13,37 +13,8 @@ async def on_ready():
     print("Bot Is Working and Online!")
     print('--------------------------')
 
-async def getting_audio(sink: discord.sinks, channel: discord.TextChannel, *args):
-    recorded_users = [
-        f"<@{user_id}>"
-        for user_id, audio in sink.audio_data.items()
-    ]
-    await sink.vc.disconnect()
-    files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in sink.audio_data.items()]
-    await channel.send(f"finished recording audio for: {', '.join(recorded_users)}.", files=files)
-
-@bot.command()
-async def start_recording_vc(ctx):
-    voice = ctx.author.voice
-    if not voice:
-        await ctx.respond("Currently not in a VC!")
-    vc = await voice.channel.connect()
-    connections.update({ctx.guild.id:vc})
-    vc.start_recording(
-        discord.sinks.WaveSink(),
-        getting_audio,
-        ctx.channel
-    )
-    await ctx.respond("Currently recording!")
-
-@bot.command()
-async def stop_recording_vc(ctx):
-    if ctx.guild.id in connections:
-        vc = connections[ctx.guild.id]
-        vc.stop_recording()
-        del connections[ctx.guild.id]
-        await ctx.delete()
-    else:
-        await ctx.respond("Currently not in a VC!")
+@bot.command
+async def hello(ctx,*,user_input:str):
+    await ctx.send(user_input)
 
 bot.run(discord_token)
