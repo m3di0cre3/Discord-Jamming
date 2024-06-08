210b3e649cb5b5c07454a7b91bb60dda0ea77f54
suoeh <dylan.x4.com@gmail.com>
2024-06-08 00:08:08 -0400

    cleaned up website
    meow

--- webstitecolon3/app.py
+++ webstitecolon3/app.py
@@ -25,13 +25,5 @@ def get_data():
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/game')
-def game():
-    return render_template('game.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
