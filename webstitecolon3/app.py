25c06e6b7f8c8f8e9a152c0e4e5b9d9e2acdbd67
Owen Wu <owenwdd01@gmail.com>
2024-06-09 08:48:16 -0400

    final commit maybe

--- webstitecolon3/app.py
+++ webstitecolon3/app.py
@@ -1,14 +1,18 @@
 from flask import *
 from flask_apscheduler import APScheduler
-from flask_socketio import SocketIO,emit
+from flask_socketio import SocketIO,emit, disconnect
 from random import choice
+import pyaudio
 import requests
 import base64
 import json
+import socket
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'secret!'
 socketio = SocketIO(app)
+
 
 counter = 0
 received_data = "loading!"
@@ -19,6 +23,10 @@ lyrics = {0:'i',1:'wanna',2:'eat',3:'something'}
 piano_notes = ['A','B','C','D','E','F','G','H','I']
 cur_lyric_index = 0
 cur_piano_index = 0
+finished_vocal_game = False
+finished_guitar_game = False
+finished_piano_game = False
+
 
 @app.route('/choose_song', methods=['GET'])
 def choose_song_menu():
@@ -41,9 +49,22 @@ def get_piano_note():
     global cur_piano_index
     if cur_piano_index < len(piano_notes)-1:
         cur_piano_index += 1
+        finished_piano_game = False
+    else:
+        finished_piano_game = True
+        cur_piano_index = 0
+
     socketio.emit("piano_note",{"data":piano_notes[cur_piano_index]})
 
 # buttons ish
+
+@app.route('/play_menu/end_screen')
+def end_game():
+    if finished_piano_game or finished_vocal_game or finished_guitar_game:
+        return render_template('end_screen.html')
+    else:
+        return redirect(url_for('play_game_menu'))
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -63,9 +84,7 @@ def guitar_game():
 @app.route('/play_menu/piano_game')
 def piano_game():
     return render_template('piano_game.html')
-@app.route('/play_menu/end_screen')
-def end_game():
-    return render_template('end_screen.html')
+
 
 
 # progress code
@@ -101,6 +120,57 @@ def receive_song():
 def get_song():
     return jsonify(song)
 
+# record mic for matlab
+audio = pyaudio.PyAudio()
+do_record = False
+stream = None
+sock = None
+
+# mp3Player = vlc.MediaPlayer("SubfolderForMicInputStuff/cmajorscale.mp3")
+notPlayed = True
+
+@app.route('/beforerec')
+def beforerec():
+    return render_template('recording.html', recording=False)
+
+@app.route('/record')
+def duringrec():
+    global stream, do_record,sock
+
+    stream = audio.open(format=pyaudio.paFloat32, channels=1, rate=44100, input=True, frames_per_buffer=1470)
+    do_record = True
+
+    # Establish socket connection to MATLAB
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('localhost', 12346))
+
+    return render_template('recording.html', recording=True, feedback="")
+
+@app.route('/endrec')
+def endrec():
+    global do_record, stream, sock
+    do_record = False 
+    stream.stop_stream()
+    stream.close()
+    sock.close()
+
+    return render_template('recording.html', recording=False)
+
+def record():
+    global do_record, stream, sock, notPlayed
+    if do_record:
+        data = stream.read(1470)
+        try:
+            sock.sendall(data)  # Send raw binary data
+            if notPlayed:
+                # mp3Player.play()
+                notPlayed = False
+            # sock.sendall(np.frombuffer(data, dtype=np.float32))
+            # print(np.frombuffer(data, dtype=np.float32)) 
+            print(data)
+        except ConnectionResetError:
+            pass
+
 # jfdklf
 if __name__ == '__main__':
     counter += 1
@@ -108,5 +178,6 @@ if __name__ == '__main__':
     scheduler.add_job(func=get_music_feedback,trigger='interval',id='job',seconds = 1)
     scheduler.add_job(func=get_lyrics,trigger='interval',id='job2',seconds = 1)
     scheduler.add_job(func=get_piano_note,trigger='interval',id='jo32',seconds = 1)
+    scheduler.add_job(func=record, trigger='interval', id="job4", seconds=0.033, max_instances=10)
     scheduler.start()
     app.run(debug=True)
